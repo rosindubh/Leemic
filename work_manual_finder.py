dc0ce544980884a027fb12d028d422893b6adfd8
rosindubh <p.welsby2@ntlworld.com>
2019-04-19 17:13:43 +0100

    added more manuals

--- work_manual_finder.py
+++ work_manual_finder.py
@@ -8,18 +8,21 @@ import webbrowser
 # program to find manuals for machines at work
 # phil welsby 17 april 2019
 
-# to add a Sharp Handy Guide
+# to add a Sharp Manual
 # first add all the model number to the list called MODELS
-# next add the same model numbers to their own list inside the sharp_handy() function
-# add an if statement to get the model_number variable entered by the user using the or (boolean)
+# next add the same model numbers to their own list inside the sharp function (handy/service/parts)
+# create a list with the model numbers for the manual you are setting up
+# add an if statement to get the model_number variable entered by the user
 # then add a webbrowser.open_new('<absolute path'>) to open the pdf
 # finally add an input() request to halt the program followed by the main() function
 
 # set variables
 MAKE = ['SHARP', 'SAMSUNG', 'LEXMARK']
 SHARP_TYPE = ['HANDY', 'SERVICE', 'PARTS']
-MODELS = ['999', '3050', '3550', '4050', '5050', '6050', '3060', '3070', '3560', '3570', '4060',
-           '4070', '5070', '6070', '6240', '7040', '6500', '7500', '6580', '7580', 'C4150', 'C6160', 'M5255', 'M5270']
+SAMSUNG_TYPE = ['SERVICE', 'PARTS']
+MODELS = ['999', '654', '754', '904', '1054', '1204', '1055', '1205', '3050', '3550', '4050', '5050', '6050', '3060', '3070', '3560', '3570', '4060',
+          '4070', '5070', '6070', '6240', '7040', '6500', '7500', '6580', '7580', '6170', '5170', 
+          '4170', '6150', '5150', '4150', '3650', '3150', '2650', 'C4150', 'C6160', 'M5255', 'M5270']
 
 # function to clear screen
 def wiper():
@@ -62,6 +65,8 @@ def sharp_handy():
     PHOENIX = ['3060', '3070', '3560', '3570', '4060', '4070', '5070', '6070']
     POLARIS = ['6240', '7040', '6500', '7500']
     POLARIS2 = ['6580', '7580']
+    HERCULES = ['904', '1054', '1204', '1055', '1205']
+    TAURUS = ['654', '754']
     if model_number in GRIFFIN:
         webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-5050/Handy-Guide-Griffin-MX3050N-MX6050N.pdf')
         input('Enter to continue...')
@@ -78,6 +83,14 @@ def sharp_handy():
         webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-6580/MX-6580_7580_handy_guide.pdf')
         input('Enter to continue...')
         main()
+    elif model_number in HERCULES:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MX-M904_HG.pdf')
+        input('Enter to continue...')
+        main()
+    elif model_number in TAURUS:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M754/Handy-Guide-Taurus-MXM654-MXM754.pdf')
+        input('Enter to continue...')
+        main()
     elif model_number == '999':
         main()
 # sharp service function
@@ -89,6 +102,8 @@ def sharp_service():
     SERVICE_3 = ['6580', '7580']
     SERVICE_4 = ['6500', '7500']
     SERVICE_5 = ['6240', '7040']
+    HERCULES = ['904', '1054', '1204']
+    TAURUS = ['654', '754']
     print('\t\t\t\tEnter the model number')
     model_number = input('\t\t\t\t>>>')
     if model_number not in MODELS:
@@ -115,6 +130,14 @@ def sharp_service():
         webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 6240-7040/MX-6240_service_manual.pdf')
         input('Enter to continue...')
         main()
+    elif model_number in HERCULES:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MXM904-M1054-M1204_SM.pdf')
+        input('Enter to continue...')
+        main()
+    elif model_number in TAURUS:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M754/MXM654-M754_SM.pdf')
+        input('Enter to continue...')
+        main()
     elif model_number == '999':
         main()
 
@@ -123,11 +146,42 @@ def sharp_service():
 # sharp parts function
 def sharp_parts():
     wiper()
-    print('this is the Sharp parts function')
+    SERVICE_1 = ['6070', '5070', '4070', '3570', '3070', '4060', '3560', '3060', '6050', '5050', '4050', 
+                 '3550', '3050','6170', '5170', '4170', '6150', '5150', '4150', '3650', '3150', '2650']
+    SERVICE_2 = ['6500', '7500']
+    HERCULES = ['904', '1054', '1204']
+    TAURUS = ['654', '754']
+    print('\t\t\t\tEnter the model number')
+    model_number = input('\t\t\t\t>>>')
+    if model_number not in MODELS:
+        print('No Parts Manual available for', model_number)
+        sleep(3)
+        sharp_parts()
+    if model_number in SERVICE_1:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-5050/MX5050-MX6050_parts.pdf')
+        input('Enter to continue...')
+        main()
+    elif model_number in SERVICE_2:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 6500-7500/MX-6500_parts.pdf')
+        input('Enter to continue...')
+        main()
+    elif model_number in HERCULES:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MXM904-M1054-M1204_PG.pdf')
+        input('Enter to continue...')
+        main()
+    elif model_number in TAURUS:
+        webbrowser.open_new('/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M754/MXM654-MXM754-KB13_PG_.pdf')
+        input('Enter to continue...')
+        main()
+    elif model_number == '999':
+        main()
+
 
 # samsung function
 def samsung():
-    print('need to write the samsung function to find model')
+    print('need to write the samsung function to find manual type')
+    input('Enter to continue...')
+    main()
 
 # lexmark function
 def lexmark():
