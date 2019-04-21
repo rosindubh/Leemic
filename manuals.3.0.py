a211a5fdfbf4ffebca988f75a3bdd6a5a832801f
rosindubh <p.welsby2@ntlworld.com>
2019-04-21 10:18:45 +0100

    added service manuals

--- manuals.3.0.py
+++ manuals.3.0.py
@@ -1,6 +1,10 @@
-# 3rd attempt
+# 3rd attempt at writing a manual finder
+# this is much simpler
+# it uses a key to find the path to the manual
+# and then uses that path to open the pdf
 # 20 april 2019
 
+from time import sleep
 import os
 import webbrowser
 
@@ -30,7 +34,64 @@ request = {'6240H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 65
           'M3570H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_HandyGuide.pdf',
           'M4070H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_HandyGuide.pdf',
           'M5070H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_HandyGuide.pdf',
-          'M6070H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_HandyGuide.pdf'}
+          'M6070H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_HandyGuide.pdf',
+          'M654H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M754/Handy-Guide-Taurus-MXM654-MXM754.pdf',
+          'M754H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M754/Handy-Guide-Taurus-MXM654-MXM754.pdf',
+          'M904H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MX-M904_HG.pdf',
+          'M1054H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MX-M904_HG.pdf',
+          'M1204H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MX-M904_HG.pdf',
+          'M1055H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MX-M904_HG.pdf',
+          'M1205H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MX-M904_HG.pdf',
+          'M365H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M565/Handy-Guide-Orion-Lotus-MXM365N-MXM465N-MXM565N.pdf',
+          'M465H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M565/Handy-Guide-Orion-Lotus-MXM365N-MXM465N-MXM565N.pdf',
+          'M565H': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M565/Handy-Guide-Orion-Lotus-MXM365N-MXM465N-MXM565N.pdf',
+
+          '6240S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 6240-7040/MX-6240_service_manual.pdf',
+          '7040S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 6240-7040/MX-6240_service_manual.pdf',
+          '6500S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 6500-7500/MX6500-7500_SM_latest.pdf',
+          '7500S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 6500-7500/MX6500-7500_SM_latest.pdf',
+          '3050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '3550S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '4050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '3060S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '3560S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '4060S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '3070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '3570S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '4070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX 3070-3570-4070/Service Manual (Revised February 2016).pdf',
+          '5070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-5050/MX-5050_service_manual.pdf',
+          '6070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-5050/MX-5050_service_manual.pdf',
+          '5050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-5050/MX-5050_service_manual.pdf',
+          '6050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-5050/MX-5050_service_manual.pdf',
+          '6580S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-6580/MX-6580_7580_service_manual.pdf',
+          '7580S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-6580/MX-6580_7580_service_manual.pdf',
+          '7090S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-7090_8090/MX-7090_8090_service.pdf',
+          '8090S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX Colour/MX-7090_8090/MX-7090_8090_service.pdf',
+          'M3070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M3570S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M4070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M5070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M6070S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M3050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M3550S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M4050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M5050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M6050S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M3070/MX-M3070_service_manual.pdf',
+          'M654S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M754/MXM654-M754_SM.pdf',
+          'M754S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M754/MXM654-M754_SM.pdf',
+          'M904S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MXM904-M1054-M1204_SM.pdf',
+          'M1054S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MXM904-M1054-M1204_SM.pdf',
+          'M1204S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M904/MXM904-M1054-M1204_SM.pdf',
+          'M1055S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M1205/MX-M1205_service_manual.pdf',
+          'M1205S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX-M1205/MX-M1205_service_manual.pdf',
+          'M365S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',
+          'M364S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',
+          'M465S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',
+          'M464S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',
+          'M565S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',
+          'M564S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',
+          'M465S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',
+          'M565S': '/media/phil/Phil_Welsby/Manufacturers/Sharp/MX-B-W/MX M364/MX M364-365N Service Manual.pdf',}
 
 
 # function to clear screen
@@ -62,6 +123,7 @@ def main():
     H = Handy Guide
     S = Sevice Manual
     P = Parts Catalogue\n\n''')
+
     print('''    Enter the model number.
     supperceeded by an 'M' 
     if it is a mono machine.
@@ -70,18 +132,38 @@ def main():
     eg: 3070H would request the MX-3070 handy guide
        M3070H would request the MX-M3070 handy guide\n\n''')
 
-    try:
-        choice = input('>>> ')
-        choice = choice.upper()
-        manual = request[choice]
-        webbrowser.open_new(manual)
-        input('Enter to continue...')
+#    try:
+#        choice = input('>>> ')
+#        choice = choice.upper()
+#        manual = request[choice]
+#        webbrowser.open_new(manual)
+#        input('Enter to continue...')
+#        wiper()
+#        main()
+#    except:
+#        print('No manual for', choice)
+#        input('Enter to continue...')
+#        wiper()
+#        main()
+
+    choice = input('>>> ')
+    if choice == '999':
+        print('Goodbye...')
+        sleep(3)
         wiper()
-        main()
-    except:
-        print('No manual for', choice)
-        input('Enter to continue...')
-        wiper()
-        main()
+    else:
+        try:
+            choice = choice.upper()
+            manual = request[choice]
+            webbrowser.open_new(manual)
+            input('Enter to continue...')
+            wiper()
+            main()
+        except:
+            print('No manual for', choice)
+            input('Enter to continue...')
+            wiper()
+            main()
+
 
 main()
