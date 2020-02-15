08cdb190681f7fbd945e7b2b2066721f8005ae10
rosindubh <p.welsby2@ntlworld.com>
2020-02-15 19:01:00 +0000

    first commit

---
+++ misc/copy_count_calc.py
@@ -0,0 +1,16 @@
+# program to work out copy count between a previous vist and current
+# 15 feb 2020 - phil welsby
+
+old_bk = int(input('Enter previous black counter: '))
+old_col = int(input('Enter previous colour counter: '))
+new_bk = int(input('Enter current black meter: '))
+new_col = int(input('Enter current colour meter: '))
+
+total_bk = new_bk - old_bk
+total_col = new_col - old_col
+
+total_count = total_bk + total_col
+
+print('Total Black  =', total_bk)
+print('Total Colour =', total_col)
+print('Total Count  =', total_count)
